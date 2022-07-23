d5fa1454b13de2c47db31902d88073e27c1cf941
Tan Tran <2188362+tant@users.noreply.github.com>
2022-07-24 00:00:14 +0700

    to chuc template

--- base/views.py
+++ base/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page ne')
+    return render(request,'main.html')
+
+def newhome(request):
+    return render(request,'home.html')
 
 def contactus(request):
     return HttpResponse('contact us')
