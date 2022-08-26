5ba2e2487553d96de40be97b19f45c7d4fb0bc74
Phuc Pham <lammphuc2031@gmail.com>
2022-08-26 09:28:08 +0700

    sua lai noi dung

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
 
     path('ajax/ajax-load-more-2.html', TemplateView.as_view(template_name='ajax-load-more-2.html'), name="loadmore2"),
     path('ajax/ajax-load-more-3.html', TemplateView.as_view(template_name='ajax-load-more-3.html'), name="loadmore3"),
-    path('ajax/ajax-services-detail.html', TemplateView.as_view(template_name='ajax-services-detail.html'), name="servicedetail"),
+    path('ajax/ajax-services-customapp.html', TemplateView.as_view(template_name='ajax-services-customapp.html'), name="servicecustomapp"),
+    path('ajax/ajax-services-outsource.html', TemplateView.as_view(template_name='ajax-services-outsource.html'), name="servicedoutsource"),
     path('ajax/ajax-team-detail.html', TemplateView.as_view(template_name='ajax-team-detail.html'), name="teamdetail"),
     path('ajax/blog-ajax-load-more-2.html', TemplateView.as_view(template_name='blog-ajax-load-more-2.html'), name="blogloadmore2"),
     path('ajax/blog-ajax-load-more-3.html', TemplateView.as_view(template_name='blog-ajax-load-more-3.html'), name="blogloadmore3"),
